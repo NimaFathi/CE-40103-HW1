16bc73e811c1d1d4a3c60068fc4520bc24c72721
Niwgoat <nimafathinimafathi@gmail.com>
2021-03-07 21:24:57 +0330

    Init project files

---
+++ Twitter/urls.py
@@ -0,0 +1,27 @@
+"""Twitter URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import include
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/account/', include('account_management.urls_v1')),
+    path('api/v1/tweet/', include('post_management.urls_tweets_v1')),
+    path('api/v1/comment/', include('post_management.urls_v1_comments')),
+    path('api/v2/post/', include('post_management.urls_v2')),
+
+]
